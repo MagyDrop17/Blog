4c017cebaf2bb11b147b9b1433c1b700c7aa76bb
MagyDrop17 <dvalenciamarin@gmail.com>
2023-08-17 00:07:22 +0200

    Added the selectAll and findById with JPA

--- src/main/java/cat/nebula/blog/MainBlogApp.java
+++ src/main/java/cat/nebula/blog/MainBlogApp.java
@@ -1,7 +1,14 @@
 package cat.nebula.blog;
 
+import cat.nebula.blog.customer.Customer;
+import cat.nebula.blog.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MainBlogApp {
@@ -9,5 +16,27 @@ public class MainBlogApp {
     public static void main(String[] args) {
         SpringApplication.run(MainBlogApp.class, args);
     }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+
+            Customer alex = new Customer(
+                    "Alex",
+                    "alex@nebula.cat",
+                    25
+            );
+
+            Customer maria = new Customer(
+                    "Maria",
+                    "maria@nebula.cat",
+                    30
+            );
+
+            List<Customer> customers = List.of(alex, maria);
+            customerRepository.saveAll(customers);
+        };
+
+    };
 
 }
