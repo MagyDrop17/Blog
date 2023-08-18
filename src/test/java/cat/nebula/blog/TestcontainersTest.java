e78cbb5fcbcfcdfeec71cb2b0b0ada57ee5bec38
MagyDrop17 <dvalenciamarin@gmail.com>
2023-08-18 02:12:26 +0200

    Intro to testing and TestContainers

---
+++ src/test/java/cat/nebula/blog/TestcontainersTest.java
@@ -0,0 +1,26 @@
+package cat.nebula.blog;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestcontainersTest extends AbstractTestcontainers {
+
+
+    @Test
+    void canStartPostgresDb() {
+
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        //assertThat(postgreSQLContainer.isHealthy()).isTrue();
+
+    }
+
+
+}
